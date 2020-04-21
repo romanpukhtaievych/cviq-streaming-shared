4fbb639b941d18721891a0f819042daa4584d818
Roman Pukhtaievych <roman.pukhtaievych@ring.com>
2020-04-21 16:11:01 +0300

    refactoring

--- gap_1_score.py
+++ gap_1_score.py
@@ -3,12 +3,11 @@ import numpy as np
 MAX_SCORE = 40
 P = 4
 
-def get_gap_1_score(gaps, fps=False):
+def get_gap_1_score(gaps, fps=30):
     """implementation of GAP_1_SCORE from https://confluence.atl.ring.com/display/CVIQ/GAP+Score
     gaps - list of all the gaps between frames
 
     GAP_1_SCORE requires only a list of all the gaps
-    scaled GAP_1_SCORE and GAP_2_SCORE require the frame rate of the input video.
     """
     g = np.array(gaps)
     g1 = g[g <= 200]
@@ -20,18 +19,17 @@ def get_gap_1_score(gaps, fps=False):
 
     if len(g1) != 0:
         w1 = w(g1)
-        s1 = w1.sum() / g1.shape * g1.sum() / g.sum()
+        s1 = sum(w1) / len(g1) * sum(g1) / sum(g)
     if len(g2) != 0:
         w2 = (1 - (g2 - 200) / (g2 + 200) ) * (P - 1) / (P + 1)
-        s2 = w2.sum() /  g2.shape * g2.sum() / g.sum()
+        s2 = sum(w2) /  len(g2) * sum(g2) / sum(g)
 
     score = MAX_SCORE * (s1 + s2)
 
     if fps:
         scale = 1 / w(1000/fps)
-        score = score[0] * scale
+        score = score * scale
 
-    print(f' {round(score, 2)}')
     return round(score, 2)
 
 # # USAGE
